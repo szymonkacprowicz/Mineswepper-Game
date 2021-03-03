4a0f40e42c2ae22f6d1b1a87a244779f5662db38
szymonkacprowicz <szymonkrzysztof11@gmail.com>
2021-03-03 15:32:13 +0100

    first commit

---
+++ src/com/codegym/games/snake/Snake.java
@@ -0,0 +1,68 @@
+package com.codegym.games.snake;
+
+import com.codegym.engine.cell.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Snake {
+
+    private final static String HEAD_SIGN = "\uD83D\uDC7E";
+    private final static String BODY_SIGN = "\u26AB";
+
+    private List<GameObject> snakeParts = new ArrayList<>();
+    public boolean isAlive = true;
+    private Direction direction = Direction.LEFT;
+
+    public Snake(int x, int y) {
+        snakeParts.add(new GameObject(x, y));
+        snakeParts.add(new GameObject(x + 1, y));
+        snakeParts.add(new GameObject(x + 2, y));
+    }
+
+    public void draw(Game game) {
+        Color color = isAlive ? Color.BLACK : Color.RED;
+
+        for (int i = 0; i < snakeParts.size(); i++) {
+            GameObject part = snakeParts.get(i);
+            String sign = (i != 0) ? BODY_SIGN : HEAD_SIGN;
+            game.setCellValueEx(part.x, part.y, Color.NONE, sign, color, 75);
+        }
+    }
+
+    public void move() {
+        GameObject newHead = createNewHead();
+        if (newHead.x >= SnakeGame.WIDTH
+                || newHead.x < 0
+                || newHead.y >= SnakeGame.HEIGHT
+                || newHead.y < 0) {
+            isAlive = false;
+            return;
+        }
+
+        snakeParts.add(0, newHead);
+        removeTail();
+    }
+
+    public GameObject createNewHead(){
+        GameObject oldHead = snakeParts.get(0);
+        if (direction == Direction.LEFT) {
+            return new GameObject(oldHead.x - 1, oldHead.y);
+        } else if (direction == Direction.RIGHT) {
+            return new GameObject(oldHead.x + 1, oldHead.y);
+        } else if (direction == Direction.UP) {
+            return new GameObject(oldHead.x, oldHead.y - 1);
+        } else {
+            return new GameObject(oldHead.x, oldHead.y + 1);
+        }
+    }
+
+    public void removeTail() {
+        snakeParts.remove(snakeParts.size() - 1);
+
+    }
+
+    public void setDirection(Direction direction) {
+        this.direction = direction;
+    }
+}
---
+++ src/com/codegym/games/snake/SnakeGame.java
@@ -0,0 +1,42 @@
+package com.codegym.games.snake;
+
+import com.codegym.engine.cell.*;
+
+public class SnakeGame extends Game {
+
+    public static final int WIDTH = 15;
+    public static final int HEIGHT = 15;
+
+    private Snake snake;
+
+    private int turnDelay;
+
+    @Override
+    public void initialize() {
+        setScreenSize(WIDTH, HEIGHT);
+        createGame();
+    }
+
+    private void createGame() {
+        turnDelay = 300;
+        setTurnTimer(turnDelay);
+        snake = new Snake(WIDTH / 2, HEIGHT / 2);
+        drawScene();
+    }
+
+    private void drawScene() {
+        for (int i = 0; i < WIDTH; i++) {
+            for (int j = 0; j < HEIGHT; j++) {
+                setCellValueEx(i, j, Color.DARKSEAGREEN, "");
+            }
+        }
+        snake.draw(this);
+    }
+
+    @Override
+    public void onTurn(int step) {
+        snake.move();
+        drawScene();
+
+    }
+}
